268039c465bdff3cc71d8b6c604573bc279bc41a
kornflakes <61383309+kornflakes@users.noreply.github.com>
2020-05-18 22:17:34 -0300

    Included TODOs and README

--- PYDiceGame.py
+++ PYDiceGame.py
@@ -3,31 +3,44 @@ import time
 
 
 def text_delay(text, delay: float):
-    ''' This function is used to print the text character by character'''
+    '''
+    TODO - MAKE BETTER DOCSTRING AND PUT THE FUNCTION IN THE TEXTS FILE
+    This function is used to print the text slowly, character by character
+    '''
     for char in text:
         time.sleep(delay)
         print(char, end='', flush=True)
 
 
 def dice_roll():
-    '''Function used to roll the dice (print random dice numbers)'''
+    '''
+    TODO - CREATE BETTER FUNCTION (ADD MORE DICE TYPES); MAKE COMBAT FILE
+    Function used to roll the dice (print random dice numbers)
+    '''
     d6 = random.randint(1, 6)
     return d6
 
 
 '''
+TODO - PUT TEXT IN ANOTHER FILE
 Text history - learn to take text from file(CSV, JSON, excel, etc)
 '''
 intro_text1 = 'There was once a man known for his luck.'
 intro_text2 = 'It is not known if the man himself was lucky, or if his dice was cursed.'
-intro_text3 = 'All that is known is that his man never lost a game of dice.'
-intro_text4 = 'He was called'
+intro_text3 = 'All that is known is that this man never lost a game of dice.'
+intro_text4 = 'He was called:'
 intro_text5 = 'The Dice J'
 
 
 def game_start():
-    '''Game loop; the loop will go on until the player type anything else than yes'''
+    '''
+    TODO - CREATE GAME MENU
+    Game loop; the loop will go on until the player type anything else than yes
+    '''
     while True:
+        '''
+        TODO - PUT INTRO IN A FUNCTION (IN THE TEXTS FILE)
+        '''
         text_delay(intro_text1, 0.1)
         print('')
         time.sleep(2)
@@ -50,5 +63,7 @@ def game_start():
             break
 
 
-'''Call the game loop function'''
+'''
+Call the game loop function
+'''
 game_start()
---
+++ README.md
@@ -0,0 +1,33 @@
+# A Game of Dice
+
+## Description
+
+This is a small python game I'm making. It's a text-based adventure.
+
+The idea came when I was searching for small python projects for beginners to step up my python skills, and since the simple dice game project was just meant to print random numbers like rolling a dice, I thought about challenging myself in creating this.
+
+So the main idea is pretty simple: make a little text-based adventure with a tiny story, and building it up with more elements until I'm satisfied with the end product.
+
+Of course, the focus is not to turn this in a AAA game and win awards, but a little thing to make me better at python (somehow).
+
+## The game
+
+Still in the 'alpha' phase, but it'll play like this:
+
+You're a dude that has some luck in games of dice, and will play against other dudes in games of dice.
+
+While you'll play the game, you'll know more about the history (pretty cheesy, so don't bother about that).
+
+I'll make some gameplay elements like upgrades, life, NPC dialogs, skills, menus, save system, etc. (also, think about a better name for it).
+
+## Expected release date
+
+Soon™
+
+But really, this is a side project, and as I have work, chores and other things to do, don't expect a release date on this.
+
+As I'll progress through the project, I'll add the changes in the top of this README, like a changelog.
+
+## Closing
+
+As always, I appreciate constructive feedback. Thanks in advance!
